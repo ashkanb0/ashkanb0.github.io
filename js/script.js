a111bbf138579ad5aa8e8021080c458ab6711478
Ashkan <ashkan.dant3@gmail.com>
2014-09-06 19:09:48 +0430

    prettifying complete, just fill it!

--- js/script.js
+++ js/script.js
@@ -5,6 +5,52 @@ function calculate_seconds(){
 	$('#age').html(Math.floor(secs))
 }
 
+function gotopart(letter){
+	$('html, body').animate({
+		scrollTop: $('#jump-'+letter).position().top
+	},1000)
+}
+
 $(function () {
 	var interval = setInterval(calculate_seconds, 1000);
+	$('.part-link').each(function(){
+		$(this).click(function(){
+			var part_id = $(this).find('span span').html().toLowerCase()
+			gotopart(part_id);
+		})
+	})
+	$(document).keypress(function(event){
+		var letter = String.fromCharCode(event.which)
+		gotopart(letter);
+	})
+	$(document).scroll(function(){
+		$('.part-link').each(function(){
+				var color;
+				var part_id = $(this).find('span span').html().toLowerCase()
+				var part = $('#jump-'+part_id)
+				var top = part.position().top
+				pos = $(document).scrollTop()
+				var color_threshoold = $(document).height() /10.0;
+				var dist = top - pos;
+
+				if (dist <= -2 * color_threshoold)
+					color = '#bbb2af';
+				if (dist <= -1 * color_threshoold  && dist > -2 * color_threshoold)
+					color = '#5eb0cf';
+				if (dist <= color_threshoold  && dist > -1 * color_threshoold)
+					color = '#00adef';
+				if (dist <= 2* color_threshoold  && dist > color_threshoold)
+					color = '#2faedf';
+				if (dist <= 4* color_threshoold  && dist > 2*color_threshoold)
+					color = '#8cb1bf';
+				if (dist > 4 * color_threshoold)
+					color = '#bbb2af';
+
+				$(this).find('span').css('color', color)
+		})
+	})
+
+
+
+	$('.part-link')[0].click()
 })
